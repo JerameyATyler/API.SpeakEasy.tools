69f28b7b94e4d07e2a354780fd96c6e7cd562ded
theanthonychen <theanthonychen@gmail.com>
2020-04-14 13:55:55 -0400

    Basic API Layout

--- src/services/server.js
+++ src/services/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const jwt = require('express-jwt');
-const jwks = require('jwks-rsa');
+const jwksRsa = require('jwks-rsa');
+const jwtAuthz = require('express-jwt-authz');
 const cors = require('cors');
 const morgan = require('morgan');
 const { SERV_PORT } = process.env;
@@ -9,45 +10,53 @@ require('dotenv').config();
 
 let app = express();
 app.use(express.json());
-app.use(cors({
-    credentials: true,
-    origin: true,
-}));
+const corsOptions =  {
+  origin: 'http://localhost:3000'
+};
+app.use(cors(corsOptions));
 
-app.use(morgan('tiny'));
-app.disable('x-powered-by');
+// app.use(morgan('tiny'));
+// app.disable('x-powered-by');
+
+const checkJwt = jwt({
+  // Dynamically provide a signing key based on the [Key ID](https://tools.ietf.org/html/rfc7515#section-4.1.4) header parameter ("kid") and the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),  // Validate the audience and the issuer.
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  algorithms: ['RS256']
+});
+const checkScopes = jwtAuthz(['read:messages']);
+// app.use(jwtCheck);
+
+app.get('/api/public', function(req, res) {
+  res.json({
+    message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
+  });
+});app.get('/api/private', checkJwt, function(req, res) {
+  res.json({
+    message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+  });
+});app.get('/api/private-scoped', checkJwt, checkScopes, function(req, res) {
+  res.json({
+    message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
+  });
+});app.use(function(err, req, res, next){
+  console.error(err.stack);
+  return res.status(err.status).json({ message: err.message });
+});
 
 app.use((err, req, res, next) => {
-    if(err) {
-        console.error(err.message);
-        console.error(err.stack);
-        return res.status(err.output.statusCode || 500).json(err.output.payload);
-    }
+  if(err) {
+      console.error(err.message);
+      console.error(err.stack);
+      return res.status(err.output.statusCode || 500).json(err.output.payload);
+  }
 });
-
-let jwtCheck = jwt({
-    secret: jwks.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: `${process.env.JWKS_URI}`
-    }),
-    audience: `${process.env.AUTH0_AUDIENCE}`,
-    issuer: `${process.env.AUTH0_ISSUER}`,
-    algorithms: ['RS256']
-});
-
-app.use(jwtCheck);
-
-app.get('/storage', function (req, res) {
-    res.send({storage: 'stored'});
-});
-
-
-// let port = process.env.PORT || 8080;
-
-// app.listen(port);
-
 
 let serverInstance;
 
